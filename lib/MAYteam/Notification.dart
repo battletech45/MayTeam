c6ecb23ddd531aedd15e40081da699e794d4c975
Altay Taneri <taneri862@gmail.com>
2023-01-13 02:03:20 +0300

    Example notification added

---
+++ lib/MAYteam/Notification.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+
+  if (message.data.containsKey('data')) {
+    // Handle data message
+    final data = message.data['data'];
+  }
+
+  if (message.data.containsKey('notification')) {
+    // Handle notification message
+    final notification = message.data['notification'];
+  }
+  // Or do other work.
+}
+
+class FCM {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  final streamCtlr = StreamController<String>.broadcast();
+  final titleCtlr = StreamController<String>.broadcast();
+  final bodyCtlr = StreamController<String>.broadcast();
+
+  setNotifications() {
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+    FirebaseMessaging.onMessage.listen(
+          (message) async {
+        if (message.data.containsKey('data')) {
+          // Handle data message
+          streamCtlr.sink.add(message.data['data']);
+        }
+        if (message.data.containsKey('notification')) {
+          // Handle notification message
+          streamCtlr.sink.add(message.data['notification']);
+        }
+        // Or do other work.
+        titleCtlr.sink.add(message.notification.title);
+        bodyCtlr.sink.add(message.notification.body);
+      },
+    );
+    // With this token you can test it easily on your phone
+    final token =
+    _firebaseMessaging.getToken().then((value) => print('Token: $value'));
+  }
+
+  dispose() {
+    streamCtlr.close();
+    bodyCtlr.close();
+    titleCtlr.close();
+  }
+}
+
+class NotificationPage extends StatefulWidget {
+  @override
+  State<NotificationPage> createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+
+  String notificationTitle = 'No Title';
+  String notificationBody = 'No Body';
+
+  @override
+  void initState() {
+    final _firebaseMessaging = FCM();
+    _firebaseMessaging.setNotifications();
+
+    _firebaseMessaging.bodyCtlr.stream.listen(_changeBody);
+    _firebaseMessaging.titleCtlr.stream.listen(_changeTitle);
+
+    super.initState();
+  }
+
+  _changeBody(String msg) => setState(() => notificationBody = msg);
+  _changeTitle(String msg) => setState(() => notificationTitle = msg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              notificationTitle,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              notificationBody,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
