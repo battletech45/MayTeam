49c28cd389eb9c1d87ac62205c1c5961153bd58a
Altay Taneri <taneri862@gmail.com>
2023-02-13 03:51:23 +0300

    New page implemented for login forget password

---
+++ lib/MAYteam/ResetPasswordPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordPage extends StatefulWidget {
+  @override
+  State<ResetPasswordPage> createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends State<ResetPasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     
+   );
+  }
+}
