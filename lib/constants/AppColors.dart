fcf2e1f8ba6b59ad495347fe1297ffe0c4dd374a
Altay Taneri <taneri862@gmail.com>
2023-09-21 17:25:20 +0300

    Colors added to AppColor class

---
+++ lib/constants/AppColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static  Color? mainColor = Colors.brown[900];
+  static  Color? backgroundColor = Colors.grey[850];
+  static Color? iconColor = Colors.white;
+  static Color? primaryButtonColor = Colors.red[900];
+  static Color? createButtonColor = Colors.brown[700];
+
+}
