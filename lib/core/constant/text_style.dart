cf9fd99ef0726310fe1d20435841e52a30d2dab7
Altay Taneri <taneri862@gmail.com>
2024-06-25 09:08:18 +0300

    updated text style colors

--- lib/core/constant/text_style.dart
+++ lib/core/constant/text_style.dart
@@ -1,3 +1,4 @@
+import 'package:MayTeam/core/constant/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -17,39 +18,39 @@ class AppTextStyle {
   static TextStyle dialogText = TextStyle(fontSize: 14.sp);
 
   static TextStyle fieldText = TextStyle(fontSize: 11.sp);
-  static TextStyle fieldHint = TextStyle(fontSize: 11.sp, color: AppColors.fieldHintGrey);
+  static TextStyle fieldHint = TextStyle(fontSize: 11.sp, color: AppColor.iconColor);
   static TextStyle fieldError = TextStyle(fontSize: 11.sp);
   static TextStyle fieldTitle = TextStyle(fontSize: 12.sp);
-  static TextStyle formCardTitle = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500, color: AppColors.secondary);
+  static TextStyle formCardTitle = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500, color: AppColor.secondaryTextColor);
 
   static TextStyle settingTile = TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500);
 
-  static TextStyle sliderChip = TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w300, color: AppColors.textWhite);
-  static TextStyle sliderTitle = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, color: AppColors.textWhite);
-  static TextStyle sliderDesc = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w300, color: AppColors.textWhite.withOpacity(0.9));
+  static TextStyle sliderChip = TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w300, color: AppColor.primaryBackgroundColor);
+  static TextStyle sliderTitle = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, color: AppColor.primaryBackgroundColor);
+  static TextStyle sliderDesc = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w300, color: AppColor.primaryBackgroundColor.withOpacity(0.9));
 
   static TextStyle mainSubtitle = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500);
-  static TextStyle subtitleTextButton = TextStyle(fontSize: 13.sp, color: AppColors.main);
+  static TextStyle subtitleTextButton = TextStyle(fontSize: 13.sp, color: AppColor.primaryTextColor);
 
   static TextStyle smallCardTitle = TextStyle(fontSize: 10.sp, fontWeight: FontWeight.w500);
   static TextStyle smallCardDesc = TextStyle(fontSize: 8.sp, fontWeight: FontWeight.w300);
 
   static TextStyle drawerButton = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500);
 
-  static TextStyle textPageHeader = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500, color: AppColors.textWhite);
+  static TextStyle textPageHeader = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500, color: AppColor.primaryBackgroundColor);
 
-  static TextStyle imagePageHeaderTitle = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500, color: AppColors.textWhite);
-  static TextStyle imagePageHeaderDesc = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w300, color: AppColors.textWhite.withOpacity(0.8));
+  static TextStyle imagePageHeaderTitle = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500, color: AppColor.primaryBackgroundColor);
+  static TextStyle imagePageHeaderDesc = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w300, color: AppColor.primaryBackgroundColor.withOpacity(0.8));
 
   static TextStyle blogText = TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w300);
 
   static TextStyle chipText = TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w300);
 
   static TextStyle notificationTitle = TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w500);
-  static TextStyle notificationDesc = TextStyle(fontSize: 10.sp, fontWeight: FontWeight.w300, color: AppColors.textGreyAbout.withOpacity(0.8));
+  static TextStyle notificationDesc = TextStyle(fontSize: 10.sp, fontWeight: FontWeight.w300, color: AppColor.iconColor.withOpacity(0.8));
 
-  static TextStyle contactCardTitle = TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500, color: AppColors.textWhite);
+  static TextStyle contactCardTitle = TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500, color: AppColor.primaryBackgroundColor);
 
-  static TextStyle imageError = TextStyle(fontSize: 11.sp, color: AppColors.main);
-  static TextStyle imageErrorCode = TextStyle(fontSize: 11.sp, color: AppColors.textGrey);
+  static TextStyle imageError = TextStyle(fontSize: 11.sp, color: AppColor.red);
+  static TextStyle imageErrorCode = TextStyle(fontSize: 11.sp, color: AppColor.iconColor);
 }
