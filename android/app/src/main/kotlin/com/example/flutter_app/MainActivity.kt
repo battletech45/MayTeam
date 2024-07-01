707e4a8645c5c1424bd4ef0aa179bdacb4e6a14c
Altay Taneri <taneri862@gmail.com>
2024-07-01 16:41:54 +0300

    Revert "made necessary changes for notifications"
    This reverts commit 9475aea8f6838637cfaf4aade10ab848763b4686.

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.flutter_app
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {}
+class MainActivity: FlutterActivity() {
+}
--- lib/core/service/notification.dart
+++ lib/core/service/notification.dart
@@ -73,8 +73,8 @@ class NotificationService {
   NotificationDetails notificationDetail() {
     return const NotificationDetails(
         android: AndroidNotificationDetails(
-          'mayteam',
-          'mayteam',
+          'smartform',
+          'smartform',
           importance: Importance.max,
         ),
         iOS: DarwinNotificationDetails(),
